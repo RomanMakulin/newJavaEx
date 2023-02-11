43636805450198801d45fc93136e3cc2f2e0e58a
unknown <WayWayz>
2023-02-11 12:53:31 +0300

    добавлены try catch

--- src/main/java/homeWorkSem1Ex1.java
+++ src/main/java/homeWorkSem1Ex1.java
@@ -3,12 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class homeWorkSem1Ex1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Введите число : ");
-        int num = Integer.parseInt(reader.readLine());
-        reader.close();
-        System.out.println("Треугольное число = " + Triangle(num) + "\n");
+    public static void main(String[] args){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Введите число : ");
+            int num = Integer.parseInt(reader.readLine());
+            reader.close();
+            System.out.println("Треугольное число = " + Triangle(num) + "\n");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public static int Triangle(int num) {
--- src/main/java/massivChetEl.java
+++ src/main/java/massivChetEl.java
@@ -7,37 +7,44 @@ import java.util.Random;
 // дан массив чисел, нужно сформировать новую выборку
 // только из четных чисел
 public class massivChetEl {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
         int count = 0;
         int newSize = 0;
 
         // ввод с клавиатуры
-        BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
-        System.out.println("Введите размер массива: ");
-        int size = Integer.parseInt(reader.readLine());
-        reader.close();
-        int[] newArr = new int[size];
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
+            System.out.println("Введите размер массива: ");
+            int size = Integer.parseInt(reader.readLine());
+            reader.close();
+            int[] newArr = new int[size];
 
-        // заполнение массива рандомом
-        for (int i = 0; i < newArr.length; i++) {
-            Random random = new Random();
-            newArr[i] = random.nextInt(1, 10);
-        }
 
-        // проверяет условие задачи и формирует новый размер для след массива
-        for (int i = 0; i < newArr.length; i++) {
-            if (newArr[i] % 2 == 0)
-                newSize++;
-        }
+            // заполнение массива рандомом
+            for (int i = 0; i < newArr.length; i++) {
+                Random random = new Random();
+                newArr[i] = random.nextInt(1, 10);
 
-        // формируем итоговый массив
-        int[] resArr = new int[newSize];
-        for (int i = 0; i < newArr.length; i++) {
-            if (newArr[i] % 2 == 0)
-                resArr[count++] = newArr[i];
+            }
+
+            // проверяет условие задачи и формирует новый размер для след массива
+            for (int i = 0; i < newArr.length; i++) {
+                if (newArr[i] % 2 == 0)
+                    newSize++;
+            }
+
+            // формируем итоговый массив
+            int[] resArr = new int[newSize];
+            for (int i = 0; i < newArr.length; i++) {
+                if (newArr[i] % 2 == 0)
+                    resArr[count++] = newArr[i];
+            }
+            System.out.println(Arrays.toString(newArr));
+            System.out.println(Arrays.toString(resArr));
         }
-        System.out.println(Arrays.toString(newArr));
-        System.out.println(Arrays.toString(resArr));
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
